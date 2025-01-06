206e5dc9db6eae76261e2b970a770d967bc83bb1
ramamimu <ahm.f.ramadhani@gmail.com>
2025-01-06 21:16:14 +0700

    feat(nexcent): client section

---
+++ src/components/nexcent/clients/ClientSection.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="container mx-auto my-10 flex flex-col px-5 py-10">
+    <div class="flex flex-col items-center gap-5 py-10 font-inter">
+      <p class="text-5xl font-semibold text-slate-700 md:text-6xl">
+        Our Clients
+      </p>
+      <p class="text-center text-lg font-light md:text-xl">
+        We have been working with some Fortune 500+ clients
+      </p>
+    </div>
+    <div class="my-5 flex flex-row flex-wrap justify-around gap-2">
+      <img
+        v-for="n in 6"
+        :key="n"
+        :src="`/src/assets/nexcent/images/client-logo-${n}.png`"
+        alt=""
+      />
+    </div>
+  </section>
+</template>
--- src/views/NexcentView.vue
+++ src/views/NexcentView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import HeaderSection from "@/components/nexcent/header/HeaderSection.vue";
 import HeroSection from "@/components/nexcent/hero/HeroSection.vue";
+import ClientSection from "@/components/nexcent/clients/ClientSection.vue";
 </script>
 
 <template>
   <HeaderSection />
   <HeroSection />
+  <ClientSection />
 </template>
